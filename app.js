2786a62b0c1b2201b0417843a3923ed2c6735977
MaquinoBSC <martinare.ipn@gmail.com>
2021-10-27 15:26:39 -0500

    Implementar barra de navegacion y agregar funcion onAuthStateChanged de firebase auth para mostrar el cambio de menu de acuerdo al estado de autenticacion del usuario

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("hola aqui");
+const botones= document.querySelector('#botones');
+const nombreUsuario= document.querySelector('#nombreUsuario');
+
+firebase.auth().onAuthStateChanged((user)=> {
+    if(user){
+        console.log(user);
+        botones.innerHTML= /*html*/`
+            <button class="btn btn-outline-danger">Cerrar Sesion</button>
+        `;
+    }
+    else{
+        console.log("No existe user");
+        botones.innerHTML= /*html*/`
+            <button class="btn btn-outline-success mr-3">Acceder</button>
+        `;
+    }
+});
